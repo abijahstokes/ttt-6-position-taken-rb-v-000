6d1dd75b0f05e5f5bc9582398af965bea64f71be
Abijah Stokes <abijahstokes@gmail.com>
2018-09-18 19:04:09 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,11 +2,9 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  elsif board == "X"
-    return false
-  else board == "O"
-    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
   end
 end
 
-array = ["index", "number"]
+
